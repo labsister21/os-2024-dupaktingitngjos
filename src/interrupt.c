91e8ab73ead6dec99d5841db2d37d4e8dc6383ab
Akmal2205 <syarafiakll2004@gmail.com>
2024-04-04 12:32:15 +0700

    feat : add interrupt

---
+++ src/interrupt.c
@@ -0,0 +1,64 @@
+#include "header/cpu/interrupt.h"
+
+
+// Activate PIC mask for keyboard only
+void activate_keyboard_interrupt(void){
+    out(PIC1_DATA, in(PIC1_DATA) & ~(1 << IRQ_KEYBOARD));
+}
+
+// I/O port wait, around 1-4 microsecond, for I/O synchronization purpose
+void io_wait(void){
+    out(0x80, 0);
+}
+
+// Send ACK to PIC - @param irq Interrupt request number destination, note: ACKED_IRQ = irq+PIC1_OFFSET
+void pic_ack(uint8_t irq){
+    if (irq >= 8) out(PIC2_COMMAND, PIC_ACK);
+    out(PIC1_COMMAND, PIC_ACK);
+}
+
+// Shift PIC interrupt number to PIC1_OFFSET and PIC2_OFFSET (master and slave)
+void pic_remap(void){
+    // Starts the initialization sequence in cascade mode
+    out(PIC1_COMMAND, ICW1_INIT | ICW1_ICW4); 
+    io_wait();
+    out(PIC2_COMMAND, ICW1_INIT | ICW1_ICW4);
+    io_wait();
+    out(PIC1_DATA, PIC1_OFFSET); // ICW2: Master PIC vector offset
+    io_wait();
+    out(PIC2_DATA, PIC2_OFFSET); // ICW2: Slave PIC vector offset
+    io_wait();
+    out(PIC1_DATA, 0b0100); // ICW3: tell Master PIC, slave PIC at IRQ2 (0000 0100)
+    io_wait();
+    out(PIC2_DATA, 0b0010); // ICW3: tell Slave PIC its cascade identity (0000 0010)
+    io_wait();
+
+    out(PIC1_DATA, ICW4_8086);
+    io_wait();
+    out(PIC2_DATA, ICW4_8086);
+    io_wait();
+
+    // Disable all interrupts
+    out(PIC1_DATA, PIC_DISABLE_ALL_MASK);
+    out(PIC2_DATA, PIC_DISABLE_ALL_MASK);
+}
+
+/**
+ * Main interrupt handler when any interrupt / exception is raised.
+ * DO NOT CALL THIS FUNCTION.
+ * 
+ * This function will be called first if any INT 0x00 - 0x40 is raised, 
+ * and will call proper ISR for respective interrupt / exception.
+ * 
+ * If inter-privilege interrupt raised, SS and ESP is automatically out of main_interrupt_handler()
+ * parameter. Can be checked with ((int*) info) + 4 for user $esp, 5 for user $ss
+ * 
+ * Again, this function is not for normal function call, all parameter will be automatically set when interrupt is called.
+ * @param frame Information about CPU during interrupt is raised
+ */
+void main_interrupt_handler(struct InterruptFrame frame){
+    switch (frame.int_number) {
+        // ...
+    }
+
+}
--- src/kernel.c
+++ src/kernel.c
@@ -4,6 +4,8 @@
 #include "header/kernel-entrypoint.h"
 #include "header/text/framebuffer.h"
 #include "header/cpu/portio.h"
+#include "header/cpu/idt.h"
+#include "header/cpu/interrupt.h"
 
 // void kernel_setup(void) {
 //     uint32_t a;
@@ -17,27 +19,37 @@
 //     while (true);
 // }
 
-void kernel_setup(void)
-{
-    framebuffer_clear();
-    framebuffer_write(3, 8, 'A', 0, 0xA);
-    framebuffer_write(3, 9, 'B', 0, 0xB);
-    framebuffer_write(3, 10, 'C', 0, 0xC);
-    framebuffer_write(3, 11, 'D', 0, 0xD);
-    framebuffer_write(3, 12, 'E', 0, 0xE);
-    framebuffer_write(3, 13, 'F', 0, 0xF);
-    framebuffer_write(3, 14, '0', 0, 0x0);
-    framebuffer_write(3, 15, '1', 0, 0x1);
-    framebuffer_write(3, 16, '2', 0, 0x2);
-    framebuffer_write(3, 17, '3', 0, 0x3);
-    framebuffer_write(3, 18, '4', 0, 0x4);
-    framebuffer_write(3, 19, '5', 0, 0x5);
-    framebuffer_write(3, 20, '6', 0, 0x6);
-    framebuffer_write(3, 21, '7', 0, 0x7);
-    framebuffer_write(3, 22, '8', 0, 0x8);
-    framebuffer_write(3, 23, '9', 0, 0x9);
+// void kernel_setup(void)
+// {
+//     framebuffer_clear();
+//     framebuffer_write(3, 8, 'A', 0, 0xA);
+//     framebuffer_write(3, 9, 'B', 0, 0xB);
+//     framebuffer_write(3, 10, 'C', 0, 0xC);
+//     framebuffer_write(3, 11, 'D', 0, 0xD);
+//     framebuffer_write(3, 12, 'E', 0, 0xE);
+//     framebuffer_write(3, 13, 'F', 0, 0xF);
+//     framebuffer_write(3, 14, '0', 0, 0x0);
+//     framebuffer_write(3, 15, '1', 0, 0x1);
+//     framebuffer_write(3, 16, '2', 0, 0x2);
+//     framebuffer_write(3, 17, '3', 0, 0x3);
+//     framebuffer_write(3, 18, '4', 0, 0x4);
+//     framebuffer_write(3, 19, '5', 0, 0x5);
+//     framebuffer_write(3, 20, '6', 0, 0x6);
+//     framebuffer_write(3, 21, '7', 0, 0x7);
+//     framebuffer_write(3, 22, '8', 0, 0x8);
+//     framebuffer_write(3, 23, '9', 0, 0x9);
 
-    framebuffer_set_cursor(3, 10);
-    while (true)
-        ;
+//     framebuffer_set_cursor(3, 10);
+//     while (true)
+//         ;
+// }
+
+void kernel_setup(void) {
+    load_gdt(&_gdt_gdtr);
+    pic_remap();
+    initialize_idt();
+    framebuffer_clear();
+    framebuffer_set_cursor(0, 0);
+    __asm__("int $0x4");
+    while (true);
 }
