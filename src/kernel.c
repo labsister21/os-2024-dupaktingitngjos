16fec125faca2651b2247c5513c3e72e14b4b45c
root <root@LAPTOP-PS0T0LBM>
2024-04-22 12:05:30 +0700

    add: chapter 2.2

--- src/kernel.c
+++ src/kernel.c
@@ -57,65 +57,65 @@
 //     while (true);
 // }
 
-void kernel_setup(void) {
-    load_gdt(&_gdt_gdtr);
-    pic_remap();
-    activate_keyboard_interrupt();
-    initialize_idt();
-    activate_keyboard_interrupt();
-    framebuffer_clear();
+// void kernel_setup(void) {
+//     load_gdt(&_gdt_gdtr);
+//     pic_remap();
+//     activate_keyboard_interrupt();
+//     initialize_idt();
+//     activate_keyboard_interrupt();
+//     framebuffer_clear();
 
-    /* ----------
-        Keyboard
-       ---------- */
-    int col = 0;
-    int row = 0;
-    keyboard_state_activate();
-    while (true) {
-         char c;
-         get_keyboard_buffer(&c);
-         framebuffer_set_cursor(row, col);
+//     /* ----------
+//         Keyboard
+//        ---------- */
+//     int col = 0;
+//     int row = 0;
+//     keyboard_state_activate();
+//     while (true) {
+//          char c;
+//          get_keyboard_buffer(&c);
+//          framebuffer_set_cursor(row, col);
 
-         if (c)
-         {
-            if (c == '\n')
-            {
-                if (row != FRAMEBUFFER_HEIGHT - 1)
-                {
-                    row++; 
-                    col = 0;
-                }
-            }
-            else if (c == '\b')
-            {
-                --col;
-                if (col < 0)
-                {
-                    col = FRAMEBUFFER_WIDTH - 1;
-                    if (row > 0) --row;
-                }
-                framebuffer_write(row, col, ' ', 0xF, 0);
-            }
-            else 
-            {
-                if (!(row == FRAMEBUFFER_HEIGHT - 1 && col == FRAMEBUFFER_WIDTH))
-                    framebuffer_write(row, col++, c, 0xF, 0);
-            }
+//          if (c)
+//          {
+//             if (c == '\n')
+//             {
+//                 if (row != FRAMEBUFFER_HEIGHT - 1)
+//                 {
+//                     row++; 
+//                     col = 0;
+//                 }
+//             }
+//             else if (c == '\b')
+//             {
+//                 --col;
+//                 if (col < 0)
+//                 {
+//                     col = FRAMEBUFFER_WIDTH - 1;
+//                     if (row > 0) --row;
+//                 }
+//                 framebuffer_write(row, col, ' ', 0xF, 0);
+//             }
+//             else 
+//             {
+//                 if (!(row == FRAMEBUFFER_HEIGHT - 1 && col == FRAMEBUFFER_WIDTH))
+//                     framebuffer_write(row, col++, c, 0xF, 0);
+//             }
 
-            if (col == FRAMEBUFFER_WIDTH)
-            {
-                if (row != FRAMEBUFFER_HEIGHT - 1)
-                {
-                    col = 0;
-                    ++row;
-                }
-            }
-         }
-    }
+//             if (col == FRAMEBUFFER_WIDTH)
+//             {
+//                 if (row != FRAMEBUFFER_HEIGHT - 1)
+//                 {
+//                     col = 0;
+//                     ++row;
+//                 }
+//             }
+//          }
+//     }
 
-    __asm__("int $0x4");
-    while (true);
-}
+//     __asm__("int $0x4");
+//     while (true);
+// }
 
 // void kernel_setup(void) {
 //     load_gdt(&_gdt_gdtr);
@@ -130,3 +130,34 @@ void kernel_setup(void) {
 //     write_blocks(&b, 17, 1);
 //     while (true);
 // }
+
+void kernel_setup(void) {
+    load_gdt(&_gdt_gdtr);
+    pic_remap();
+    initialize_idt();
+    activate_keyboard_interrupt();
+    framebuffer_clear();
+    framebuffer_set_cursor(0, 0);
+    initialize_filesystem_fat32();
+    gdt_install_tss();
+    set_tss_register();
+
+    // Allocate first 4 MiB virtual memory
+    paging_allocate_user_page_frame(&_paging_kernel_page_directory, (uint8_t*) 0);
+
+    // Write shell into memory
+    struct FAT32DriverRequest request = {
+        .buf                   = (uint8_t*) 0,
+        .name                  = "shell",
+        .ext                   = "\0\0\0",
+        .parent_cluster_number = ROOT_CLUSTER_NUMBER,
+        .buffer_size           = 0x100000,
+    };
+    read(request);
+
+    // Set TSS $esp pointer and jump into shell 
+    set_tss_kernel_current_stack();
+    kernel_execute_user_program((uint8_t*) 0);
+
+    while (true);
+}
